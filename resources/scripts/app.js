d616c066c54d3643a915fd9ccffb81192189eb9d
Noitidart <noitidart@gmail.com>
2015-09-12 01:03:41 -0700

    1.0-night.rev1
    Making some progress, getting first set of parents

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -39,6 +39,7 @@ var	ANG_APP = angular.module('ccedgetraversal-graph', [])
 		
 		MODULE.start_str = '[gc.marked] JS Object (ArrayBuffer)';
 		MODULE.log_path = 'C:\\Users\\Vayeate\\AppData\\Local\\Temp\\cc-edges.5828.1442035908.log';
+		MODULE.graphviz_code = 'asd';
 		
 		MODULE.BrowseSelectFile = function(aArgName) {
 			var fp = Cc['@mozilla.org/filepicker;1'].createInstance(Ci.nsIFilePicker);
@@ -53,5 +54,81 @@ var	ANG_APP = angular.module('ccedgetraversal-graph', [])
 			}// else { // cancelled	}
 		};
 	}]);
+
+var lastReadLogPath = '';
+var lastReadLogContents = '';
+function readLogAndGetCode() {
+	
+	var step0 = function() {
+		// check if should read, and set lastReadLogContents
+		if (lastReadLogPath != gAngScope.BC.log_path) {
+			lastReadLogPath = gAngScope.BC.log_path;
+			var promise_read = OS.File.read(gAngScope.BC.log_path, {encoding:'utf-8'});
+			promise_read.then(
+				function(aVal) {
+					console.log('Fullfilled - promise_read - ');
+					// start - do stuff here - promise_read
+					// alert('ok read it:' + aVal);
+					lastReadLogContents = aVal;
+					step1();
+					// end - do stuff here - promise_read
+				},
+				function(aReason) {
+					var rejObj = {name:'promise_read', aReason:aReason};
+					console.warn('Rejected - promise_read - ', rejObj);
+					alert('Failed to read file due to error, see browser console');
+					// deferred_createProfile.reject(rejObj);
+				}
+			).catch(
+				function(aCaught) {
+					var rejObj = {name:'promise_read', aCaught:aCaught};
+					console.error('Caught - promise_read - ', rejObj);
+					alert('dev error on read of log file');
+					// deferred_createProfile.reject(rejObj);
+				}
+			);
+		} else {
+			step1();
+		}
+	};
+	
+	var step1 = function() {
+		// start parsing to get code
+		// alert('starting parse');
+		var graphviz_code = [];
+		graphviz_code.push('digraph finite_state_machine {');
+		graphviz_code.push('\trankdir=LR;');
+		
+		var strPatt = '^.*?([A-Z0-9]+) .*?' + escapeRegExp(gAngScope.BC.start_str) + '.*?$';
+		alert(strPatt);
+		var patt = new RegExp(strPatt, 'gm');
+		
+		var parentAddresses = [];
+		var match;
+		while (match = patt.exec(lastReadLogContents)) {
+			graphviz_code.push('\t"' + match[1] + '" [label=" ' + match[0] + '"]');
+			parentAddresses.push(match[1]);
+		}
+		
+		
+		graphviz_code.push('}');
+		
+		gAngScope.BC.graphviz_code = graphviz_code.join('\n');
+		gAngScope.$digest();
+	};
+	
+	step0();
+
+}
+
+// start - common helper functions
+function escapeRegExp(text) {
+	if (!arguments.callee.sRE) {
+		var specials = ['/', '.', '*', '+', '?', '|', '(', ')', '[', ']', '{', '}', '\\'];
+		arguments.callee.sRE = new RegExp('(\\' + specials.join('|\\') + ')', 'g');
+	}
+	return text.replace(arguments.callee.sRE, '\\$1');
+}
+// end - common helper functions
 
 document.addEventListener('DOMContentLoaded', onPageReady, false);
